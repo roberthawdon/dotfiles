7ce48a400eb3cb83aeb26b433b20d88f5a03bffd
Robert Hawdon <git@robertianhawdon.me.uk>
2023-03-28 00:26:05 +0100

    Started adding last modified data

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -284,6 +284,59 @@
 (use-package ox-gemini)
 
 (after! org
+
+  (defun rh/org-find-time-file-property (property &optional anywhere)
+    "Return the position of the time file PROPERTY if it exists.
+When ANYWHERE is non-nil, search beyond the preamble."
+    (save-excursion
+      (goto-char (point-min))
+      (let ((first-heading
+             (save-excursion
+               (re-search-forward org-outline-regexp-bol nil t))))
+        (when (re-search-forward (format "^#\\+%s:" property)
+                                 (if anywhere nil first-heading)
+                                 t)
+          (point)))))
+
+  (defun rh/org-has-time-file-property-p (property &optional anywhere)
+    "Return the position of time file PROPERTY if it is defined.
+As a special case, return -1 if the time file PROPERTY exists but
+is not defined."
+    (when-let ((pos (rh/org-find-time-file-property property anywhere)))
+      (save-excursion
+        (goto-char pos)
+        (if (and (looking-at-p " ")
+                 (progn (forward-char)
+                        (org-at-timestamp-p 'lax)))
+            pos
+          -1))))
+
+  (defun rh/org-set-time-file-property (property &optional anywhere pos)
+    "Set the time file PROPERTY in the preamble.
+When ANYWHERE is non-nil, search beyond the preamble.
+If the position of the file PROPERTY has already been computed,
+it can be passed in POS."
+    (when-let ((pos (or pos
+                        (rh/org-find-time-file-property property))))
+      (save-excursion
+        (goto-char pos)
+        (if (looking-at-p " ")
+            (forward-char)
+          (insert " "))
+        (delete-region (point) (line-end-position))
+        (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
+          (insert now)))))
+
+  (defun rh/org-set-last-modified ()
+    "Update the LAST_MODIFIED file property in the preamble."
+    (interactive)
+    (when (derived-mode-p 'org-mode)
+      (rh/org-set-time-file-property "LAST_MODIFIED")))
+
+
+)
+
+(after! org
     (defun rh/gorg-colors-doom-one ()
     "Enable Doom One colors for Org headers."
     (interactive)
