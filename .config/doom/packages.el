6633eeb2d83bc60dfcb2a4f3e42f3a1eb65c9cbc
Robert Ian Hawdon <git@robertianhawdon.me.uk>
2024-06-24 13:10:32 +0100

    Added slack mode

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -29,6 +29,9 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
+(package! emacs-slack
+  :recipe (:host github :repo "isamert/emacs-slack"))
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
